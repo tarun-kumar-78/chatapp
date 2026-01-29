d9ecc6195b88b309e2770fd86901fda77ae25ae9
User2003r <itarun78072@gmail.com>
2026-01-29 11:07:46 +0530

    fix: update production URL check to use correct hostname

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -2,7 +2,6 @@
 const BACKEND_PROD_URL = "https://chatapp-wdpc.onrender.com";
 const BACKEND_LOCAL_URL = "http://localhost:5001";
 
-const isProd = window.location.hostname.includes("chatapp-snowy-psi.vercel.app");
-
+const isProd = window.location.hostname.includes("chatapp-wdpc.onrender.com");
 
 export const PROD = isProd ? BACKEND_PROD_URL : BACKEND_LOCAL_URL;
