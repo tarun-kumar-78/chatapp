4e5d85110122679879c0063ae7be3e7f91556646
User2003r <itarun78072@gmail.com>
2026-01-29 15:22:23 +0530

    fix: set secure cookie flag to true for all environments

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -5,7 +5,7 @@ const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "1d" });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: NODE_ENV !== "DEVELOPMENT",
+    secure: true,
     sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
