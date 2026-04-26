b397e8b59d17abdce1cc7966380b9b665dd014ae
Tarun Kumar <95030406+tarun-kumar-78@users.noreply.github.com>
2026-04-26 13:21:19 +0530

    Revert "refactor the desing of login and register page"

--- frontend/src/components/chats/Users.tsx
+++ frontend/src/components/chats/Users.tsx
@@ -2,14 +2,21 @@ import img from '@/assets/chatapp-image.jpg';
 import { Input } from "@/components/ui/input";
 import api from '@/service/axios';
 import type { RootState } from '@/store';
-import { setConversationId, setMessages, setSelectedUser, setUnreadCount } from '@/store/user/userSlice';
+import { addUser, setConversationId, setMessages, setSelectedUser, setUnreadCount } from '@/store/user/userSlice';
 import type { User } from '@/type/user';
-import { LogOut, Search } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { LogOut, Pencil, Search } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Badge } from "@/components/ui/badge"
-import { setLogout } from '@/store/auth/authSlice';
-
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogDescription,
+    DialogTitle,
+} from "@/components/ui/dialog"
 import {
     AlertDialog,
     AlertDialogAction,
@@ -20,7 +27,14 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
 } from "@/components/ui/alert-dialog"
-
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form"
+import { Button } from '../ui/button';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -32,9 +46,13 @@ const Users = () => {
     const { selectedUser } = useSelector((state: RootState) => state.user);
     const { user } = useSelector((state: RootState) => state.user);
     const { unreadMessagesCount } = useSelector((state: RootState) => state.user);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [previewImage, setPreviewImage] = useState<string>();
+    const [avatarFile, setAvatarFile] = useState<File | null>();
     const dispatch = useDispatch();
+    const imgRef = useRef<HTMLInputElement | null>(null);
     const navigate = useNavigate();
-    const [logout, setUserLogout] = useState<boolean>(false);
+    const [logout, setLogout] = useState<boolean>(false);
 
     useEffect(() => {
         const getUsers = async () => {
@@ -58,9 +76,9 @@ const Users = () => {
         }
     }
 
-    const getLatestMessages = async (conversationId: string) => {
+    const getMessages = async (conversationId: string) => {
         try {
-            const response = await api.get(`/api/message/getMessages/${conversationId}`);
+            const response = await api.post("/api/message/getMessages", { conversationId });
             return response.data.messages;
         } catch (err) {
             console.log("Error in getting messages", err);
@@ -81,7 +99,7 @@ const Users = () => {
         await readMessages(user.conversationId);
         const conversationId = await getConversationId(user._id);
         dispatch(setConversationId(conversationId));
-        const messages = await getLatestMessages(conversationId);
+        const messages = await getMessages(conversationId);
         dispatch(setMessages(messages));
         console.log(messages);
     }
@@ -122,11 +140,41 @@ const Users = () => {
         }
     }, [form, user]);
 
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        try {
+
+            const formData = new FormData();
+            if (avatarFile) {
+                formData.append("avatar", avatarFile);
+            }
+            formData.append("name", data.name);
+            formData.append("email", data.email);
+            setOpenDialog(false);
+            const response = await api.put("/api/user/update-profile", formData);
+            if (response.data.success) {
+                toast.success(response.data.message);
+                dispatch(addUser(response.data.user));
+            }
+        } catch (err) {
+            console.log("Error updating profile:", err);
+            toast.error("Failed to update profile.");
+        }
+    }
+
+    const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+        const previewURL = URL.createObjectURL(file);
+        setPreviewImage(previewURL);
+        setAvatarFile(file);
+
+
+    };
+
     const handleLogout = async () => {
         try {
             const response = await api.get("/api/auth/logout");
             if (response.data.success) {
-                dispatch(setLogout(false));
                 toast.success(response.data.message);
                 navigate("/login");
             }
@@ -142,11 +190,13 @@ const Users = () => {
             <div className="border bg-gray-300/30 w-full md:w-[20%] min-w-70 p-4">
                 <div className="flex justify-between items-center h-12">
                     <div className="flex items-center gap-3">
-                        <img src={user?.avatar || img} alt="profile image" className="h-9 w-9 sm:w-10 sm:h-10 rounded-full" />
+                        <img src={user?.avatar || previewImage || img} alt="profile image" className="h-9 w-9 sm:w-10 sm:h-10 rounded-full" />
                         <div className="">
                             <p className="text-sm font-medium">{user?.name}</p>
+                            <p className="text-xs">Role</p>
                         </div>
                     </div>
+                    <Pencil className="h-4 w-4 sm:w-5 sm:h-5 cursor-pointer" onClick={() => setOpenDialog(true)} />
                 </div>
                 <div className="flex my-4 border items-center px-2 rounded-full bg-white">
                     <Search className='h-4 w-4' />
@@ -185,7 +235,7 @@ const Users = () => {
                         }
                     </div>
                     <div className='flex items-center justify-center cursor-pointer'>
-                        <div className='flex gap-3 bg-black p-1 text-white items-center rounded-sm' onClick={() => setUserLogout(true)}>
+                        <div className='flex gap-3 bg-black p-1 text-white items-center rounded-sm' onClick={() => setLogout(true)}>
                             <span>Logout</span>
                             <LogOut className='h-5 w-5' />
                         </div>
@@ -194,7 +244,7 @@ const Users = () => {
                 </div>
             </div>
 
-            {/* <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+            <Dialog open={openDialog} onOpenChange={setOpenDialog}>
                 <Form {...form}>
 
                     <form id="form" onSubmit={form.handleSubmit(onSubmit)} encType="multipart/form-data" onError={() => console.log(form.getValues())}>
@@ -254,7 +304,7 @@ const Users = () => {
                         </DialogContent>
                     </form>
                 </Form>
-            </Dialog> */}
+            </Dialog>
 
             {/* Logout dialog */}
             <AlertDialog open={logout} >
@@ -265,7 +315,7 @@ const Users = () => {
                         </AlertDialogDescription>
                     </AlertDialogHeader>
                     <AlertDialogFooter>
-                        <AlertDialogCancel onClick={() => setUserLogout(false)}>Cancel</AlertDialogCancel>
+                        <AlertDialogCancel onClick={() => setLogout(false)}>Cancel</AlertDialogCancel>
                         <AlertDialogAction onClick={handleLogout}>Logout</AlertDialogAction>
                     </AlertDialogFooter>
                 </AlertDialogContent>
